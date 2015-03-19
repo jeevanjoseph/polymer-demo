1b006f0d5a24a4ee413ea15ae42a65f76f344b94
Jeevan Joseph <jeevan.joseph@oracle.com>
2015-03-19 12:07:52 -0700

    changes for monitoring and local notification

--- AiBeaconApp/ViewController/public_html/resources/js/main.js
+++ AiBeaconApp/ViewController/public_html/resources/js/main.js
@@ -1,4 +1,8 @@
 var app = {
+
+    // Setup data. We will want to call a REST service to get this array later on.
+    watchedRegions : [{id : 'Jeevan Office', uuid : '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6', major : 1, minor : 1, notifyEntryStateOnDisplay : true}],
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -17,26 +21,52 @@ var app = {
         //alert("onResume...");
     },
     onDeviceReady: function() {
-        var uuid = 'EB9AB493-32C2-4E5C-BF67-76E86E338BB9'; // mandatory
-        var identifier = 'macbookIbeacon'; // mandatory
-        var minor = 1; 
-        var major = 1; 
-
-        var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
-
+        alert('indevice ready Fired');
+        //setup delegate
+        this.setupDelegate();
+        
+        // Create beacon regions for all watched region defs and start both ranging(foreground) and monitoring(background)
+        for (var idx in watchedRegions){
+            var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(watchedRegions[idx].id, 
+                                                                    watchedRegions[idx].uuid, 
+                                                                    watchedRegions[idx].major, 
+                                                                    watchedRegions[idx].minor);
+            // startMonitoringForRegion -- startRangingBeaconsInRegion
+            cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
+                .fail('ERROR' + console.error)
+                .done();
+            // Start monitoring as well. this works in the background as well.    
+            cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+                .fail(console.error)
+                .done();
+        }
+    },
+    setupDelegate : function() {
+        alert('setupDelegate fired');
         var delegate = new cordova.plugins.locationManager.Delegate().implement({
 
             didDetermineStateForRegion: function (pluginResult) {
+            alert('didDetermineStateForRegion');
                 console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
                 setbData('#bdata', 'didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+                adf.mf.api.localnotification.add({
+                                                      "title"     :  "Title",                  // Notification title (Android ONLY)
+                                                      "alert"     :  pluginResult.state,                 // Notification alert text
+                                                      "sound"     :  "SYSTEM_DEFAULT",       // If set, the default system sound will be played
+                                                      "vibration" :  "SYSTEM_DEFAULT"        
+                                                    },
+                                                    function(request,response){alert("notification successful")},
+                                                    function(request,response){alert("notification failed")});
             },
 
             didStartMonitoringForRegion: function (pluginResult) {
+             alert('didStartMonitoringForRegion');
                console.log('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
                setbData('#bdata', 'didStartMonitoringForRegion: ' + JSON.stringify(pluginResult));
             },
 
             didRangeBeaconsInRegion: function (pluginResult) {
+                 alert('didRangeBeaconsInRegion');
                 console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
                 var beacons = pluginResult.beacons;
                 var found = false;
@@ -74,12 +104,7 @@ var app = {
 
         cordova.plugins.locationManager.setDelegate(delegate);
         //cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
-        cordova.plugins.locationManager.requestAlwaysAuthorization()
-
-        // startMonitoringForRegion -- startRangingBeaconsInRegion
-        cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
-            .fail('ERROR' + console.error)
-            .done();
+        cordova.plugins.locationManager.requestAlwaysAuthorization();    
     }
 };
 
