5dac5fc332a4ce824dd25b3a2a2ff777b70aa6c5
Jeevan Joseph <jeevan.g@gmail.com>
2015-04-01 01:50:25 -0700

    fancy build

--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,13 @@ var AUTOPREFIXER_BROWSERS = [
   'bb >= 10'
 ];
 
-var styleTask = function (stylesPath, srcs) {
+var styleTask = function(stylesPath, srcs) {
   return gulp.src(srcs.map(function(src) {
       return path.join('app', stylesPath, src);
     }))
-    .pipe($.changed(stylesPath, {extension: '.scss'}))
+    .pipe($.changed(stylesPath, {
+      extension: '.scss'
+    }))
     .pipe($.rubySass({
         style: 'expanded',
         precision: 10
@@ -38,26 +40,31 @@ var styleTask = function (stylesPath, srcs) {
     .pipe(gulp.dest('.tmp/' + stylesPath))
     .pipe($.if('*.css', $.cssmin()))
     .pipe(gulp.dest('dist/' + stylesPath))
-    .pipe($.size({title: stylesPath}));
+    .pipe($.size({
+      title: stylesPath
+    }));
 };
 
 // Compile and Automatically Prefix Stylesheets
-gulp.task('styles', function () {
+gulp.task('styles', function() {
   return styleTask('styles', ['**/*.css', '*.scss']);
 });
 
-gulp.task('elements', function () {
+gulp.task('elements', function() {
   return styleTask('elements', ['**/*.css', '**/*.scss']);
 });
 
 // Lint JavaScript
-gulp.task('jshint', function () {
+gulp.task('jshint', function() {
   return gulp.src([
       'app/scripts/**/*.js',
       'app/elements/**/*.js',
       'app/elements/**/*.html'
     ])
-    .pipe(reload({stream: true, once: true}))
+    .pipe(reload({
+      stream: true,
+      once: true
+    }))
     .pipe($.jshint.extract()) // Extract JS from .html files
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
@@ -65,18 +72,20 @@ gulp.task('jshint', function () {
 });
 
 // Optimize Images
-gulp.task('images', function () {
+gulp.task('images', function() {
   return gulp.src('app/images/**/*')
     .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true
     })))
     .pipe(gulp.dest('dist/images'))
-    .pipe($.size({title: 'images'}));
+    .pipe($.size({
+      title: 'images'
+    }));
 });
 
 // Copy All Files At The Root Level (app)
-gulp.task('copy', function () {
+gulp.task('copy', function() {
   var app = gulp.src([
     'app/*',
     '!app/test',
@@ -96,26 +105,34 @@ gulp.task('copy', function () {
     .pipe($.rename('elements.vulcanized.html'))
     .pipe(gulp.dest('dist/elements'));
 
-  return merge(app, bower, elements, vulcanized).pipe($.size({title: 'copy'}));
+  return merge(app, bower, elements, vulcanized).pipe($.size({
+    title: 'copy'
+  }));
 });
 
 // Copy Web Fonts To Dist
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
   return gulp.src(['app/fonts/**'])
     .pipe(gulp.dest('dist/fonts'))
-    .pipe($.size({title: 'fonts'}));
+    .pipe($.size({
+      title: 'fonts'
+    }));
 });
 
 // Scan Your HTML For Assets & Optimize Them
-gulp.task('html', function () {
-  var assets = $.useref.assets({searchPath: ['.tmp', 'app', 'dist']});
+gulp.task('html', function() {
+  var assets = $.useref.assets({
+    searchPath: ['.tmp', 'app', 'dist']
+  });
 
   return gulp.src(['app/**/*.html', '!app/{elements,test}/**/*.html'])
     // Replace path for vulcanized assets
     .pipe($.if('*.html', $.replace('elements/elements.html', 'elements/elements.vulcanized.html')))
     .pipe(assets)
     // Concatenate And Minify JavaScript
-    .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+    .pipe($.if('*.js', $.uglify({
+      preserveComments: 'some'
+    })))
     // Concatenate And Minify Styles
     // In case you are still using useref build blocks
     .pipe($.if('*.css', $.cssmin()))
@@ -129,11 +146,13 @@ gulp.task('html', function () {
     })))
     // Output Files
     .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'html'}));
+    .pipe($.size({
+      title: 'html'
+    }));
 });
 
 // Vulcanize imports
-gulp.task('vulcanize', function () {
+gulp.task('vulcanize', function() {
   var DEST_DIR = 'dist/elements';
 
   return gulp.src('dist/elements/elements.vulcanized.html')
@@ -143,14 +162,16 @@ gulp.task('vulcanize', function () {
       inline: true
     }))
     .pipe(gulp.dest(DEST_DIR))
-    .pipe($.size({title: 'vulcanize'}));
+    .pipe($.size({
+      title: 'vulcanize'
+    }));
 });
 
 // Clean Output Directory
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 // Watch Files For Changes & Reload
-gulp.task('serve', ['styles', 'elements'], function () {
+gulp.task('serve', ['styles', 'elements'], function() {
   browserSync({
     notify: false,
     // Run as an https by uncommenting 'https: true'
@@ -162,6 +183,11 @@ gulp.task('serve', ['styles', 'elements'], function () {
       routes: {
         '/bower_components': 'bower_components'
       }
+    },
+    ghostMode: {
+      clicks: true,
+      forms: true,
+      scroll: false
     }
   });
 
@@ -173,7 +199,7 @@ gulp.task('serve', ['styles', 'elements'], function () {
 });
 
 // Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], function () {
+gulp.task('serve:dist', ['default'], function() {
   browserSync({
     notify: false,
     // Run as an https by uncommenting 'https: true'
@@ -185,18 +211,17 @@ gulp.task('serve:dist', ['default'], function () {
 });
 
 // Build Production Files, the Default Task
-gulp.task('default', ['clean'], function (cb) {
+gulp.task('default', ['clean'], function(cb) {
   runSequence(
     ['copy', 'styles'],
-    'elements',
-    ['jshint', 'images', 'fonts', 'html'],
+    'elements', ['jshint', 'images', 'fonts', 'html'],
     'vulcanize',
     cb);
 });
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
-gulp.task('pagespeed', function (cb) {
+gulp.task('pagespeed', function(cb) {
   // Update the below URL to the public URL of your site
   pagespeed.output('example.com', {
     strategy: 'mobile',
@@ -209,7 +234,11 @@ gulp.task('pagespeed', function (cb) {
 
 // Load tasks for web-component-tester
 // Adds tasks for `gulp test:local` and `gulp test:remote`
-try { require('web-component-tester').gulp.init(gulp); } catch (err) {}
+try {
+  require('web-component-tester').gulp.init(gulp);
+} catch (err) {}
 
 // Load custom tasks from the `tasks` directory
-try { require('require-dir')('tasks'); } catch (err) {}
+try {
+  require('require-dir')('tasks');
+} catch (err) {}
